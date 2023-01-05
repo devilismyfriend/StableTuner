20c95f3a3ee94d3349a494015f2143fef545ac12
devilismyfriend <87043616+devilismyfriend@users.noreply.github.com>
2023-01-05 14:45:32 -0800

    class images resolution

--- scripts/trainer.py
+++ scripts/trainer.py
@@ -1642,7 +1642,7 @@ def main():
                         sample_dataloader, desc="Generating class images", disable=not accelerator.is_local_main_process
                     ):
                         with torch.autocast("cuda"):
-                            images = pipeline(example["prompt"]).images
+                            images = pipeline(example["prompt"],height=args.resolution,width=args.resolution).images
                         for i, image in enumerate(images):
                             hash_image = hashlib.sha1(image.tobytes()).hexdigest()
                             image_filename = class_images_dir / f"{example['index'][i] + cur_class_images}-{hash_image}.jpg"
